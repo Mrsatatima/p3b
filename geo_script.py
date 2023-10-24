c27d454eda4eaa51bd7086754817c67e3ed59fdb
Al-amin Ahmad Abbas <alaminahmadabbas@gmail.com>
2023-10-24 07:57:04 +0100

    Edit functions in geo_script

--- geo_script.py
+++ geo_script.py
@@ -4,7 +4,7 @@ import processing
 
 from helper import *
 
-path ="C:/Users/ASUS/AppData/Local/Temp/tmp4d4rmkcc.py"
+path = "C:/Users/ASUS/AppData/Local/Temp/tmp4d4rmkcc.py"
 QgsApplication.setPrefixPath(path, True)
 qgs = QgsApplication([], False)
 
@@ -12,9 +12,17 @@ qgs.initQgis()
 Processing.initialize()
 
 
-
 def crt_subset_lyr(layer,query):
-    selected = layer.getFeatures(QgsFeatureRequest().setFilterExpression(query))
+    """
+        Creates a subset of a layer by pyqgis processing 
+        selection by expression,
+        Input:
+            layer: The layer you want to get subset from (QgsVectorLayer)
+            query: Expression to be ised for querying (str)
+        Ouput:
+            memory_layer: subset of the layer (QgsVectorLayer)
+
+    """
     # Create a memory layer
     set_extent_uri = f"polygon?crs={layer.crs().authid()}"
     memory_layer = QgsVectorLayer(set_extent_uri, "set_extent", "memory")
@@ -23,7 +31,13 @@ def crt_subset_lyr(layer,query):
     memory_layer.dataProvider().addAttributes(layer.dataProvider().fields().toList())
     memory_layer.updateFields()
 
+    parameters = {'INPUT': layer,
+                  'EXPRESSION': query,
+                  'METHOD': 0,
+                  }
+    run_selection = processing.run('qgis:selectbyexpression', parameters)
     # Add the selected features to the memory layer
+    selected = run_selection['OUTPUT'].selectedFeatures()
     outFeat = QgsFeature()
     for feature in selected:
         outFeat.setGeometry(feature.geometry())
@@ -31,32 +45,22 @@ def crt_subset_lyr(layer,query):
         memory_layer.dataProvider().addFeatures([outFeat])
         memory_layer.updateExtents()
     memory_layer.commitChanges()
-
     return memory_layer
 
 
 def clip_set_ext_layer(layer, clip_layer):
-    set_extent_uri = f"polygon?crs={layer.crs().authid()}"
-    set_extent_layer = QgsVectorLayer(set_extent_uri, "set_extent", "memory")
-    set_extent_layer_data = set_extent_layer.dataProvider()
-    set_extent_layer.startEditing()
-    set_extent_layer_fields = QgsFields(layer.fields())
-    set_extent_layer_data.addAttributes(set_extent_layer_fields)
-    set_extent_layer.updateFields()
-
-    parameters = { 'INPUT' : layer,
-               'INTERSECT' : clip_layer, 
-               'METHOD' : 0, 
-               'PREDICATE' : [0] }
-    processing.run('native:selectbylocation', parameters)
-    outFeat = QgsFeature()
-    selected = layer.selectedFeatures()
-    for feature in selected:
-        outFeat.setGeometry(feature.geometry())
-        outFeat.setAttributes(feature.attributes())
-        set_extent_layer.dataProvider().addFeatures([outFeat])
-        set_extent_layer.updateExtents()
-    set_extent_layer.commitChanges()
-    return set_extent_layer
-
-
+    """
+        this function clips features that are within another layer
+        it uses the pyqgis processing clip
+        Input:
+            layer: The layer whose features needs to be clip (QgsVectorLayer)
+            clip_layer: layer for to be used for cliping (QgsVectorLayer)
+        Output:
+            clipped_layer: Clipped features (QgsVectorLayer)
+    """
+    parameters = {'INPUT': layer,
+                  'OVERLAY': clip_layer,
+                  'OUTPUT': 'TEMPORARY_OUTPUT',
+                  }
+    clipped_layer = processing.run('native:clip', parameters)
+    return clipped_layer["OUTPUT"]
