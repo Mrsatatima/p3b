de03d74dfa5a83246112fd8db55a62adea661dbe
Al-amin Ahmad Abbas <alaminahmadabbas@gmail.com>
2023-11-17 10:13:17 +0100

    Code tested and works well

--- Cluster/main.py
+++ Cluster/main.py
@@ -23,7 +23,7 @@ def main(state):
     """
     sheets = get_sheets(file_name)
     # print(sheets,len(sheets))
-    for sheet in sheets[9:]:
+    for sheet in sheets:
         # print(sheet)
         lga_list = sheet.split('.')
         try:
@@ -42,34 +42,6 @@ def main(state):
         wards_df = drop_subtotal_rows(base_data, ward_list)
         final_df = create_random_cluster(wards_df, state, lga, lga_dct, ward_dct)
         write_to_excel(final_df, f"{state}_cluster.xlsx", sheet)
-
-
-def populate_p3b_main(state):
-    """
-        this function runs the whole process for populating the p3b,
-        it calls functions from script.py,it cleans the p3b by setting
-        the actual header and populating wards and DHs. it then writes
-        to an excel file
-        Input:
-            state: Name of state you want to clean
-        Output:
-            none
-    """
-
-    sheets = get_sheets(file_name)
-    for sheet in sheets[14:]:
-        lga_list = sheet.split('.')
-        try:
-            lga = lga_list[1].strip().lower().title()
-        except:
-            lga = lga_list[0].strip().lower().title()
-        print(lga)
-        demo_df = pd.read_excel(file_name, sheet_name=sheet)
-        row_index = actual_header_row(demo_df)
-        clean_data = remove_first_blank_column(file_name, row_index, sheet)
-        ward_pop_df = populate_wards(clean_data)
-        dh_pop_df = populate_dh(ward_pop_df)
-        write_to_excel(dh_pop_df, f"{state}_p3b_populated.xlsx", sheet)
 
 
 if __name__ == "__main__":
---
+++ main.py
@@ -0,0 +1,34 @@
+from p3b import *
+
+file_name = "2023_Kwara_P3B.xlsx"
+
+def main(state):
+    """
+        this function runs the whole process for populating the p3b,
+        it calls functions from script.py,it cleans the p3b by setting
+        the actual header and populating wards and DHs. it then writes
+        to an excel file
+        Input:
+            state: Name of state you want to clean
+        Output:
+            none
+    """
+
+    sheets = get_sheets(file_name)
+    for sheet in sheets:
+        lga_list = sheet.split('.')
+        try:
+            lga = lga_list[1].strip().lower().title()
+        except:
+            lga = lga_list[0].strip().lower().title()
+        print(lga)
+        demo_df = pd.read_excel(file_name, sheet_name=sheet)
+        row_index = actual_header_row(demo_df)
+        clean_data = remove_first_blank_column(file_name, row_index, sheet)
+        ward_pop_df = populate_wards(clean_data)
+        dh_pop_df = populate_dh(ward_pop_df)
+        write_to_excel(dh_pop_df, f"{state}_p3b_populated.xlsx", sheet)
+
+
+if __name__ == "__main__":
+    main("Kwara")
